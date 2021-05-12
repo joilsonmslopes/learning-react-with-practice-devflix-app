c30cea4d6e15dcb243720b9bf70971b354c8a042
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2021-05-12 19:54:06 -0300

    Aula 01 e aula 02 ate a inicio das paginas CadastroCategoria e CadastroVideo

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/img/devflix.png";
+import { Button } from "../Button";
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <nav className='Menu'>
+      <Link as='a' to='/'>
+        <img className='Logo' src={Logo} alt='DevFlix logo' />
+      </Link>
+
+      <Button to='/cadastro/video' className='ButtonLink'>
+        Novo Video
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroVideo from "./pages/cadastro/Video";
+import CadastroCategoria from "./pages/cadastro/Categoria";
+
+const Pagina404 = () => <div>Página 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    {/* Switch - faz a lógica de verificação de rota está sendo chamada */}
+    <Switch>
+      {/* Route é como vamos declarar cada uma das paginas que temos,
+      passando o path da rota a ser chamada e qual componente vai ser chamado */}
+      <Route exact path='/' component={Home} />
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
